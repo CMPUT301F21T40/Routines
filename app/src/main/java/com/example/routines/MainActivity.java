bee52def6dcd406ba4b413199e4c5073000b2ab3
Lukas Waschuk <77379787+Napkinzz@users.noreply.github.com>
2021-10-24 14:59:51 -0600

    Fixing Merge Conflicts

--- app/src/main/java/com/example/routines/MainActivity.java
+++ app/src/main/java/com/example/routines/MainActivity.java
@@ -24,12 +24,31 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigator = findViewById(R.id.bottom_navigation);
         BottomNavigator.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
+
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int id = item.getItemId();
                 switch(id){
 
                     case R.id.home:
                         return true;
+
+            public void onClick(View v) {
+                new AddHabitFragment().show(getSupportFragmentManager(), "ADD_HABIT");
+            }
+        });
+    }
+
+    
+    // this is called from the AddHabitFragment so we can add a new Habit to the list
+    /**
+     * This is called when the + button is pressed and the info from the pop up fragment
+     * is filled out. This will add the newly created habit into the medList
+     * @param newHabit
+     */
+    public void onOkPressed(Habit newHabit){
+        habitAdapter.add(newHabit);
+    }
+
 
                     case R.id.search:
                         startActivity(new Intent(getApplicationContext(),SearchActivity.class));

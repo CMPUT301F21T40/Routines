049c91786332d7743208ff716d1c26847c08f5c2
Shanshan Wei <swei3@ualberta.ca>
2021-10-26 10:06:07 -0600

    profile updated

--- app/src/main/java/com/example/routines/ProfileActivity.java
+++ app/src/main/java/com/example/routines/ProfileActivity.java
@@ -1,26 +1,45 @@
 package com.example.routines;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import org.w3c.dom.Text;
 
 public class ProfileActivity extends AppCompatActivity {
 
     FirebaseFirestore db;
-    CollectionReference collectionReference;
+    CollectionReference Users;
     CollectionReference userNames;
     FirebaseAuth myAuth;
-
+    private String UserId;
     BottomNavigationView BottomNavigator;
+
+    TextView NameText;
+    TextView EmailText;
+    TextView UserName;
+    TextView UserEmail;
+    ImageView UserPhoto;
+    Button LogOutButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,9 +47,28 @@ public class ProfileActivity extends AppCompatActivity {
 
         myAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
-        collectionReference = db.collection("Users");
-        userNames = db.collection("User Names");
+        Users = db.collection("Users");
 
+        initializeData();
+        switchActivity();
+
+
+
+
+
+    }
+
+    public void initializeData(){
+        EmailText = findViewById(R.id.text_email_lable_profile);
+        NameText = findViewById(R.id.text_user_lable_profile);
+        UserEmail = findViewById(R.id.text_email_profile);
+        UserName = findViewById(R.id.text_user_profile);
+        UserPhoto = findViewById(R.id.image_profile);
+        LogOutButton = findViewById(R.id.button_profile);
+
+    }
+
+    public void switchActivity(){
         // The bottom Navigation bar
         BottomNavigator = findViewById(R.id.bottom_navigation);
         BottomNavigator.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
@@ -56,10 +94,19 @@ public class ProfileActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
 
+    public void showInformation(){
+        FirebaseUser User = myAuth.getCurrentUser();
+        if(User != null){
+            UserId = User.getUid();
+            Users.document(UserId).addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                @Override
+                public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
 
-        
+                }
+            });
 
-        
+        }
     }
 }

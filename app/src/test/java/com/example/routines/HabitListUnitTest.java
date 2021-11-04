d6bd1948efab2c1279c49c9f72742c0f869db48f
Lukas Waschuk <77379787+Napkinzz@users.noreply.github.com>
2021-11-04 09:13:11 -0600

    Testing getViewTest()

--- app/src/test/java/com/example/routines/HabitListUnitTest.java
+++ app/src/test/java/com/example/routines/HabitListUnitTest.java
@@ -1,9 +1,14 @@
 package com.example.routines;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
@@ -20,19 +25,22 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.testng.reporters.jq.TestNgXmlPanel;
 
 
 public class HabitListUnitTest {
-    private HabitList mockHabitList;
+    private HabitList habitList;
     private ArrayList<String> frequency = new ArrayList<>();
-    private LayoutInflater mockLayoutInflater;
     private Context mockContext = mock(Context.class);
+    private View mockView = mock(View.class);
+    private ViewGroup mockViewGroup = mock(ViewGroup.class);
+    private TextView mockTextView = mock(TextView.class);
+
 
     @BeforeEach
     public void createHabitList(){ // create a new mock habit list for every test
-        mockHabitList = new HabitList(mockContext, new ArrayList<>());
-        mockLayoutInflater = (LayoutInflater) mockContext.getSystemService(
-                Context.LAYOUT_INFLATER_SERVICE);
+        habitList = new HabitList(mockContext, new ArrayList<>());
+
     }
 
     private Habit mockHabit(){
@@ -41,61 +49,57 @@ public class HabitListUnitTest {
         return new Habit("Soccer", "It is fun", "2020-01-01", frequency, "Private");
     }
     public void addHabit(){
-        mockHabitList.addHabit(mockHabit());
+        habitList.addHabit(mockHabit());
     }
 
     @Test
     public void addHabitTest(){
-        mockHabitList.addHabit(mockHabit());
-        assertEquals(1, mockHabitList.habitCount()); // count will be 1 if the add worked
+        habitList.addHabit(mockHabit());
+        assertEquals(1, habitList.habitCount()); // count will be 1 if the add worked
     }
 
     @Test
     public void hasHabitTest(){
         Habit newHabit = mockHabit();
-        assertFalse(mockHabitList.containsHabit(newHabit)); // should be false
-        mockHabitList.addHabit(newHabit);
-        assertTrue(mockHabitList.containsHabit(newHabit)); // should be true
+        assertFalse(habitList.containsHabit(newHabit)); // should be false
+        habitList.addHabit(newHabit);
+        assertTrue(habitList.containsHabit(newHabit)); // should be true
     }
 
     @Test
     public void countHabitTest(){
-        int before = mockHabitList.habitCount(); // should be 0
-        mockHabitList.addHabit(mockHabit());
-        int after = mockHabitList.habitCount(); // should be 1
+        int before = habitList.habitCount(); // should be 0
+        habitList.addHabit(mockHabit());
+        int after = habitList.habitCount(); // should be 1
         assertEquals(0, before); // check if it equals 0
         assertEquals(1, after); // check if it equals 1
     }
     @Test
     public void getViewTest(){
-        // the line below is causing the error, LayoutInflator not mocked
+        when(mockView.findViewById(anyInt())).thenReturn(mockTextView);
 
-        //mockLayoutInflater = LayoutInflater.from(mockContext);
-        LayoutInflater layoutInflater = new MockLayoutInflater(mockLayoutInflater, mockContext);
-        View view = mockHabitList.getView(0, null, null);
-        addHabit(); // adds a habit to the habitlist
-        TextView habitName = (TextView) view.findViewById(R.id.habitName);
-        assertNotNull(habitName, "Name TextView is null ");
-        
+        addHabit(); // add a habit to the habit list
+        View view = habitList.getView(0, mockView, mockViewGroup);
+
+        // so now when we call findViewById we always get a returned mockTextView which will have no attributes? 
+        TextView name = (TextView) view.findViewById(R.id.habitName);
+        TextView reason = (TextView) view.findViewById(R.id.habitReason);
+        TextView startingDate = (TextView) view.findViewById(R.id.habitDate);
+
+        // don't get how to compare the 2, a mock cant have a value, is always null pointer
+
+        // assertEquals("Soccer", name.getText().toString()); // cannot actually compare a mock to a string
+
+        // we can check IF at least something was added to the list using assert nulls?
+        assertNotNull(name, "Name TextView is null ");
+        assertNotNull(reason, "Reason TextView is null ");
+        assertNotNull(startingDate, "Date TextView is null ");
+
+        // can check if they are mockTextViews
+        assertThat(name, is(mockTextView));
+        assertThat(reason, is(mockTextView));
+        assertThat(startingDate, is(mockTextView));
     }
 
-    // found this online but it still does not work
-    static class MockLayoutInflater extends LayoutInflater {
-        public MockLayoutInflater(Context c) {
-            super(c);
-        }
-        public MockLayoutInflater(LayoutInflater original, Context newContext) {
-            super(original, newContext);
-        }
-        @Override
-        public View onCreateView(String name, AttributeSet attrs)
-                throws ClassNotFoundException {
-            return super.onCreateView(name, attrs);
-        }
-        @Override
-        public LayoutInflater cloneInContext(Context newContext) {
-            return null;
-        }
-    }
 
 }

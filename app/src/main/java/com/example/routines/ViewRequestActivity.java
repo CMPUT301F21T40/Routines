01683526ba4ecb130fd0f08a895ab66c5bab69cc
Shanshan Wei <swei3@ualberta.ca>
2021-11-24 16:52:01 -0700

    bottom navigation bar

---
+++ app/src/main/java/com/example/routines/ViewRequestActivity.java
@@ -0,0 +1,14 @@
+package com.example.routines;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_request);
+    }
+}

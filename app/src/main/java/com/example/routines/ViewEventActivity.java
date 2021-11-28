c4cc46ed9acc2d16efb0184e78af5c1a875b5cad
Andyxzz2 <61675598+Andyxzz2@users.noreply.github.com>
2021-11-28 13:42:35 -0700

    merge

--- app/src/main/java/com/example/routines/ViewEventActivity.java
+++ app/src/main/java/com/example/routines/ViewEventActivity.java
@@ -33,7 +33,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.bumptech.glide.Glide;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;

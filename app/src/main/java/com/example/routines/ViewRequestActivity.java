12db2f43357ea6274a966e3740196373a8d8eab7
Shanshan Wei <swei3@ualberta.ca>
2021-11-29 11:58:37 -0700

    add intent for viewReuqestactivity

--- app/src/main/java/com/example/routines/ViewRequestActivity.java
+++ app/src/main/java/com/example/routines/ViewRequestActivity.java
@@ -104,11 +104,13 @@ public class ViewRequestActivity extends AppCompatActivity {
             case R.id.radio_accept:
                 updatedStatus = "accepted";
                 updateStatus(requestUserId);
+                startActivity(new Intent(this, NotificationActivity.class));
                 break;
 
             case R.id.radio_deny:
                 updatedStatus = "denied";
                 updateStatus(requestUserId);
+                startActivity(new Intent(this, NotificationActivity.class));
                 break;
 
         }

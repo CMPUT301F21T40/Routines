167168fa1d2415596df86f0649a5651bdfab72f1
Isaiah Paterson <ipaterso@ualberta.ca>
2021-11-29 14:37:19 -0700

    unit test works

--- app/src/test/java/com/example/routines/FollowingUnitTest.java
+++ app/src/test/java/com/example/routines/FollowingUnitTest.java
@@ -1,20 +1,45 @@
 package com.example.routines;
 
-import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.ArrayList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import java.util.HashMap;
 
+
 public class FollowingUnitTest {
-    private Habit mockHabit;
-    private ArrayList<String> frequency = new ArrayList<>();
-    HashMap<String, String> data;
+    HashMap<String, String> dataAccepted;
+    HashMap<String, String> dataDeclined;
+    String status;
 
     @BeforeEach
     public void createNotification(){ // create a new mock habit list for every test
-        data = new HashMap<>();
-        data.put("Sender Name", "john123");
-        data.put("Receiver Name", "emma565");
+        dataAccepted = new HashMap<>();
+        dataAccepted.put("Sender Name", "john123");
+        dataAccepted.put("Sender", "123");
+        dataAccepted.put("Receiver Name", "emma565");
+        dataAccepted.put("Receiver", "565");
+        dataAccepted.put("Status", "accepted");
+
+        dataDeclined = new HashMap<>();
+        dataDeclined.put("Sender Name", "john123");
+        dataDeclined.put("Sender", "123");
+        dataDeclined.put("Receiver Name", "emma565");
+        dataDeclined.put("Receiver", "565");
+        dataDeclined.put("Status", "pending");
+    }
+
+    @Test
+    public void checkFollowingPass() {
+        status = dataAccepted.get("Status");
+        assertEquals("accepted", status);
+    }
+
+    @Test
+    public void checkFollowingFail() {
+        status = dataDeclined.get("Status");
+        assertEquals("pending", status);
     }
 
 

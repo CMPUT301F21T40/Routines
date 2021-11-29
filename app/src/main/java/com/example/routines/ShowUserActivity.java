ec5aa9b7ce89aea19902154a20427ab890fd49f0
Shanshan Wei <swei3@ualberta.ca>
2021-11-29 11:24:29 -0700

    fix the bug

--- app/src/main/java/com/example/routines/ShowUserActivity.java
+++ app/src/main/java/com/example/routines/ShowUserActivity.java
@@ -12,6 +12,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -64,7 +65,7 @@ public class ShowUserActivity extends AppCompatActivity {
         db = FirebaseFirestore.getInstance();
         collectionReference = db.collection("Notification");
 
-        if(type == "Followers"){
+        if(type.equals("Followers")){
             showFollowers();
         }else{
             showFollowing();
